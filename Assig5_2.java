4445888a30a3e1d86e555c3cd0803079caf91d94
mokawad <31768193+mokawad@users.noreply.github.com>
2017-10-02 17:42:34 -0500

    Update Assig5_2.java

--- Assig5_2.java
+++ Assig5_2.java
@@ -22,6 +22,268 @@ class CardTable extends JFrame
    private int numCardsPerHand;
    private int numPlayers;
    public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+   {
+
+   private static String[] pnlTitles = {"High Card", 
+      "Computer Hand", "Player Hand", "Playing Area", "Time Played"};
+   
+   private JButton endGameBtn, playAgainBtn, nextRoundBtn;
+   
+   private JPanel pnlCpuHand, pnlHumanHand, pnlTimer, pnlOutput, 
+   pnlPlayArea, pnlCardsPlayed, pnlCpuCardPlayed, pnlHumanCardPlayed;
+   
+   private ArrayList<JButton> playersCardsBtns = new ArrayList<JButton>();
+   
+   // default constructor
+   // sets up the initial view, which is really
+   // the table and it's main holders
+   public GameView()
+   {
+      super();
+      setTitle(pnlTitles[0]);
+      setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+      setLayout(new BorderLayout());
+      
+      endGameBtn = new JButton("End Game");
+      playAgainBtn = new JButton("Play Again");
+      
+      // timer panel
+      pnlTimer = new JPanel();
+      
+      // cpu played Card
+      pnlCpuCardPlayed = new JPanel(new GridLayout(2, 1));
+      
+      // human played Card
+      pnlHumanCardPlayed = new JPanel(new GridLayout(2, 1));
+      
+      // area for cards played
+      pnlCardsPlayed = new JPanel(new GridLayout(1, 2));
+      pnlCardsPlayed.add(pnlCpuCardPlayed);
+      pnlCardsPlayed.add(pnlHumanCardPlayed);
+      
+      // computers cards panel
+      pnlCpuHand = new JPanel();
+      pnlCpuHand.setLayout(new GridLayout(1, 7));
+      
+      // players cards panel
+      pnlHumanHand = new JPanel();
+      pnlHumanHand.setLayout(new GridLayout(1, 7));
+      
+      // output area
+      pnlOutput = new JPanel(new GridLayout(3, 1));
+      
+      // playing area
+      pnlPlayArea = new JPanel(new GridBagLayout());
+      GridBagConstraints c = new GridBagConstraints();
+      
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.gridx = 0;
+      c.gridy = 0;
+      pnlPlayArea.add(pnlCpuHand, c);
+      
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.gridx = 0;
+      c.gridy = 1;
+      c.weighty = 1.0;
+      pnlPlayArea.add(pnlCardsPlayed, c);
+      
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.gridx = 0;
+      c.gridy = 2;
+      pnlPlayArea.add(pnlOutput, c);
+      
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.gridx = 0;
+      c.gridy = 3;
+      pnlPlayArea.add(pnlHumanHand, c);
+      
+      
+      // add all the major components to the screen
+      this.add(pnlTimer, BorderLayout.NORTH);
+      this.add(pnlPlayArea,BorderLayout.CENTER);
+      this.setSize(800, 600);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setVisible(true);
+   }
+   
+   public void playRoundScreen(String message, Hand cpuHand, 
+         Hand humanHand, int maxHandSize)
+   {
+      if(cpuHand == null || humanHand == null || 
+            maxHandSize == 0)
+         return;
+      
+      JLabel textOutput = new JLabel(message);
+      textOutput.setHorizontalAlignment(JLabel.CENTER);
+      pnlOutput.add(textOutput);
+      showCpuHand(cpuHand, maxHandSize);
+      showHumanHandAsButtons(humanHand, maxHandSize);
+      
+   }
+   
+   public void roundOutcomeScreen(String message, Hand cpuHand, Card cpuCard,
+         String cpuName, Hand humanHand, Card humanCard, String humanName, 
+         int maxHandSize)
+   {
+      if(cpuHand == null || humanHand == null || 
+            maxHandSize == 0)
+         return;
+      
+      JLabel textOutput = new JLabel(message);
+      textOutput.setHorizontalAlignment(JLabel.CENTER);
+      nextRoundBtn = new JButton("Next Round");
+      pnlOutput.add(textOutput);
+      pnlOutput.add(nextRoundBtn);
+      showCpuPlayedCard(cpuCard, cpuName);
+      showHumanPlayedCard(humanCard, humanName);
+      showCpuHand(cpuHand, maxHandSize);
+      showHumanHand(humanHand, maxHandSize);
+      
+   }
+   
+   public void gameOutcomeScreen(String message)
+   {
+      if(message == null)
+         return;
+      
+      JLabel textOutput = new JLabel(message);
+      textOutput.setHorizontalAlignment(JLabel.CENTER);
+      pnlOutput.add(textOutput);
+      pnlOutput.add(playAgainBtn);
+      pnlOutput.add(endGameBtn);
+      
+   }
+   
+   /*
+    * private helper method to repaint the UI
+    * */
+   public void clearUI()
+   {
+      pnlCpuHand.removeAll();
+      pnlCpuCardPlayed.removeAll();
+      pnlHumanHand.removeAll();
+      pnlHumanCardPlayed.removeAll();
+      pnlOutput.removeAll();
+      this.getContentPane().repaint();
+   }
+   
+   /*
+    * private helper method to repaint the UI
+    * */
+   public void rePaintUI()
+   {
+      this.getContentPane().validate();
+      this.getContentPane().repaint();
+   }
+   
+   private void showCpuHand(Hand hand, int maxHandSize)
+   {  
+      
+      for(int k = 0; k < maxHandSize; k++)
+      {
+         if(k > hand.getNumCards() - 1)
+            pnlCpuHand.add(new JLabel(GUICard.getBlankCardIcon()));
+         else
+            pnlCpuHand.add(new JLabel(GUICard.getBackCardIcon()));
+      }
+      
+   }
+   
+   private void showHumanHand(Hand hand, int maxHandSize)
+   {
+      
+      for(int k = 0; k < maxHandSize; k++)
+      {
+         if(k > hand.getNumCards() - 1)
+            pnlHumanHand.add(new JLabel(GUICard.getBlankCardIcon()));
+         else
+            pnlHumanHand.add(new JLabel(GUICard
+                  .getIcon(hand.inspectCard(k))));
+      }
+   }
+   
+   private void showHumanHandAsButtons(Hand hand, int maxHandSize)
+   {     
+      playersCardsBtns.clear();
+      for(int k = 0; k < maxHandSize; k++)
+      {
+         if(k > hand.getNumCards() - 1)
+         {
+            pnlHumanHand.add(new JLabel(GUICard.getBlankCardIcon()));  
+         }
+         else
+         {
+            JButton playCardBtn = new JButton(
+                  GUICard.getIcon(hand.inspectCard(k)));
+            playCardBtn.setBorder(BorderFactory.createEmptyBorder());
+            playCardBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            playersCardsBtns.add(playCardBtn);
+            
+            pnlHumanHand.add(playersCardsBtns.get(k));
+         }
+      }
+   }
+   
+   public void showMessageg(String title, String message)
+   {  
+      JOptionPane.showMessageDialog(
+            this,
+            message,
+            title,
+            JOptionPane.PLAIN_MESSAGE);
+   }
+   
+   public int showOptionDialog(String title, String message, String[] options)
+   {  
+      int option = JOptionPane.showOptionDialog(
+            this, 
+            message,
+            title, 
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[0]);
+      
+      return option;
+   }
+   
+   public void showCpuPlayedCard(Card card, String name)
+   {
+      pnlCpuCardPlayed.add(new JLabel(GUICard.getIcon(card)));
+      pnlCpuCardPlayed.add(new JLabel(name));
+   }
+   
+   public void showHumanPlayedCard(Card card, String name)
+   {
+      pnlHumanCardPlayed.add(new JLabel(GUICard.getIcon(card)));
+      pnlHumanCardPlayed.add(new JLabel(name));
+   }
+   
+   public void playersCardActionListener(int index, ActionListener l)
+   {
+      playersCardsBtns.get(index).setCursor(new Cursor(Cursor.HAND_CURSOR));
+      playersCardsBtns.get(index).addActionListener(l);
+   }
+   
+   public void nextRoundActionListener(ActionListener l)
+   {
+      nextRoundBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+      nextRoundBtn.addActionListener(l);
+   }
+   
+   public void endActionListener(ActionListener l)
+   {
+      endGameBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+      endGameBtn.addActionListener(l);
+   }
+   
+   public void playAgainActionListener(ActionListener l)
+   {
+      playAgainBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+      playAgainBtn.addActionListener(l);
+   }
+}
 
    // constructor to filter input and add panels to JFrame
    CardTable(String title, int numCardsPerHand, int numPlayers)
@@ -45,27 +307,103 @@ class CardTable extends JFrame
 
 class GUICard
 {
-   private static Icon[][] iconCards = new ImageIcon[14][4]; // 14 = A thru K + joker
+   private static Icon[][] iconCards = new ImageIcon[14][4];
    private static Icon iconBack;
+   private static Icon iconBlank;
    static boolean iconsLoaded = false;
 
-   // generates the image Icon array from files
+
+   /*
+    * loads the  Icons for each card value
+    * */
    static void loadCardIcons()
    {
-      
+      char[] value = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T',
+                      'J', 'Q', 'K', 'X'};
+      char[] suit = {'C', 'D', 'H', 'S'};
+      String fileName = "";
+
+      for (int i = 0; i < suit.length; i++)
+         for (int j = 0; j < value.length; j++)
+         {
+            fileName = "";
+            fileName += String.valueOf(value[j]) + String.valueOf(suit[i])
+               + ".gif";
+            iconCards[j][i] = new ImageIcon("images/" + fileName);
+         }
+      iconBack = new ImageIcon("images/BK.gif");
+      iconBlank = new ImageIcon("images/blank.gif");
+      iconsLoaded = true;
    }
 
-   // takes Card object and returns icon for that card
+   /*
+    * getter to get an Icon for a requested card
+    * */
    static public Icon getIcon(Card card)
    {
-      
+
+      if (!iconsLoaded)
+         loadCardIcons();
+
+      int cardValue = valueAsInt(card.getValue());
+      int suitValue = suitAsInt(card.getSuit());
+
+      return iconCards[cardValue][suitValue];
+   }//end method getIcon
+
+   /*
+    * getter to get a back of card icon
+    * */
+   static public Icon getBackCardIcon()
+   {
+      if (!iconsLoaded)
+         loadCardIcons();
+
+      return iconBack;
    }
 
-   // returns the card-back image
-   static public Icon getBackCardIcon() 
+   /*
+    * getter to get a blank card holder
+    * */
+   static public Icon getBlankCardIcon()
    {
-   
+      if (!iconsLoaded)
+         loadCardIcons();
+
+      return iconBlank;
    }
+
+   /*
+    * returns a cards value as an int
+    * */
+   static int valueAsInt(char cardValue)
+   {
+      int value = 0;
+
+      for (int i = 0; i < Card.cardValue.length; i++)
+         if (cardValue == Card.cardValue[i])
+         {
+            value = i;
+            break;
+         }
+
+      return value;
+   }//end method valueAsInt
+
+   /*
+    * returns a cards suit as an int
+    * */
+   static int suitAsInt(Card.Suit suit)
+   {
+      if (suit.equals(Card.Suit.CLUBS))
+         return 0;
+      else if (suit.equals(Card.Suit.DIAMONDS))
+         return 1;
+      else if (suit.equals(Card.Suit.HEARTS))
+         return 2;
+      else
+         return 3;
+   }//end method suitAsInt
 
 }
 
@@ -295,6 +633,10 @@ private int numPacks;
 // Constructor
 public Deck() 
 {
+   this.numPacks = 1;
+
+      // populate masterPack
+      allocateMasterPack();
     init(1);
 }
 
